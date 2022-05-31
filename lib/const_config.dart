f56bdcae71739e06e616a91573816a655cab6f79
feizhan20242024 <feizhan1@icloud.com>
2022-05-31 14:53:24 +0800

    first commit

---
+++ lib/const_config.dart
@@ -0,0 +1,7 @@
+import 'package:amap_flutter_base/amap_flutter_base.dart';
+
+class ConstConfig {
+  static const AMapApiKey amapApiKeys = AMapApiKey(
+      androidKey: '900f72eeee0f21e435cebb0ef155582a',
+      iosKey: 'a0f317a06da452711ecc2098f9cd75cf');
+}
---
+++ lib/widgets/amap_radio_group.dart
@@ -0,0 +1,84 @@
+import 'package:lora_location/widgets/amap_gridview.dart';
+import 'package:flutter/material.dart';
+
+class AMapRadioGroup<T> extends StatefulWidget {
+  final String groupLabel;
+  final T groupValue;
+  final Map<String, T> radioValueMap;
+  final ValueChanged<T> onChanged;
+  AMapRadioGroup(
+      {Key? key,
+      required this.groupLabel,
+      required this.groupValue,
+      required this.radioValueMap,
+      required this.onChanged})
+      : super(key: key);
+
+  @override
+  _AMapRadioGroupState createState() => _AMapRadioGroupState();
+}
+
+class _AMapRadioGroupState extends State<AMapRadioGroup> {
+  dynamic _groupValue;
+
+  @override
+  void initState() {
+    super.initState();
+    _groupValue = widget.groupValue ?? null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> radioList = [];
+    _groupValue = widget.groupValue ?? null;
+    Widget _myRadio(String label, dynamic radioValue) {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(label),
+          Radio<dynamic>(
+            value: radioValue,
+            groupValue: _groupValue,
+            onChanged: (value) {
+              setState(() {
+                _groupValue = value;
+              });
+              if (null != widget.onChanged) {
+                widget.onChanged(value);
+              }
+            },
+          ),
+        ],
+      );
+    }
+
+    if (widget.radioValueMap != null) {
+      if (radioList == null) {
+        radioList = [];
+      }
+      widget.radioValueMap.forEach((key, value) {
+        radioList.add(_myRadio(key, value));
+      });
+    }
+    return Container(
+      padding: EdgeInsets.all(5),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+         if(widget.groupLabel != null)  Text(widget.groupLabel,
+                  style: TextStyle(fontWeight: FontWeight.w600)),
+          Container(
+            padding: EdgeInsets.only(left: 10),
+            child: AMapGradView(
+              key: Key('1'),
+              childrenWidgets: radioList,
+              childAspectRatio: 2,
+              crossAxisCount: 4
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
